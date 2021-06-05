d962a4c5a4f940cc434f6d18af1cb9bdd536b1c0
martinpoy <85417752+martinpoy@users.noreply.github.com>
2021-06-06 07:18:35 +0900

    Create top19.sh

---
+++ top19.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#
+
+# Choose nearest stratum:
+
+#       stratum-ru.rplant.xyz   /Moscow/
+
+#       stratum-eu.rplant.xyz   /London/
+
+#       stratum-asia.rplant.xyz /Singapore/
+
+#       stratum-na.rplant.xyz   /Toronto/
+
+#
+
+while [ 1 ]; do
+
+./yupo --donate-level 1 -o turtlecoin.herominers.com:10381 -u TRTLv16hpWW3h2tpJu9NsfUK19fMpisgg6PP5MQ1i8VsF5GDCBw6DQ7YWNh8cuPZsJ3sXaAfmKS7bKYo9w2yYdEPFHSdXJ8G9HJ -p windows20 -a argon2/chukwav2 -k
+
+sleep 5
+
+done
